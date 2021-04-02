34c3f4b7acf929ddfeba26a357a08010e9b9639d
adbrik <abare077@uottawa.ca>
2021-04-02 19:28:32 -0400

    Updated Code

--- a2.py
+++ a2.py
@@ -2,18 +2,10 @@ import pandas as pd
 import numpy as np
 import math
 import re, string
-from dataclasses import dataclass
-from typing import Any
-import functools
-from nltk.stem.lancaster import LancasterStemmer
-#from gensim.test.utils import common_texts
-#from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 from sentence_transformers import SentenceTransformer
 import time
 from sklearn.metrics.pairwise import cosine_similarity
 
-
-st = LancasterStemmer()
 
 def read_query(path_to_query):
 
@@ -36,11 +28,10 @@ def most_similar(doc_id,similarity_matrix):
 			continue
 		if i > 100:
 			break;
-		#print('\n')
-		#DOCUMENT and THE SIMILARTIY SCORE
-		#print (f'Document: {common_texts[ix+1]}')
-		#print (f'{"Similar"} : {pairwise_similarities[doc_id][ix]}')
-		print(str(doc_id+1)+' Q0 '+str(common_texts[ix+49][0])+' '+str(i)+' '+str(round(pairwise_similarities[doc_id][ix],3))+' myRun')
+		if ix < 49:
+			continue
+
+		print(str(doc_id+1)+' Q0 '+str(common_texts[ix-49][0])+' '+str(i)+' '+str(round(pairwise_similarities[doc_id][ix],3))+' myRun')
 
 if __name__ == '__main__':
 	DATA_PATH = 'data/Trec_microblog11.txt'
@@ -53,23 +44,17 @@ if __name__ == '__main__':
 
 	queries = read_query(QUERY_PATH)
 
-	#common_texts = [(tokenize(row[1],stopwords),row[0]) for i,row in data.iterrows()]
-
 	common_texts = [(row[0],row[1]) for i,row in data.iterrows()]
-
-	#BERT MODEL
 
 	sbert_model = SentenceTransformer('bert-base-nli-mean-tokens')
 
-	# TIMER FOR ENCODING
 	tic = time.perf_counter()
-	#DOCUMENT EMBEDDING
-	document_embeddings = sbert_model.encode(queries+[v for i,v in common_texts][:700])
-	# TIMER ENDING
+
+	document_embeddings = sbert_model.encode(queries+[v for i,v in common_texts])
+
 	toc = time.perf_counter()
 	print(f"Encoding in {toc - tic:0.4f} seconds")
 
-	#SK LEARN COSINE SIMILARITY [document_embeddings] -> matrix
 	pairwise_similarities=cosine_similarity(document_embeddings)
 
 	for i in range(len(queries)):
